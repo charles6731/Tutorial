985b61ab04d8b59b10386c37bdfe0d095f76dbb8
“Charles6731” <ajerehcharles@gmail.com>
2022-08-21 08:18:53 -0500

    [add]updates

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -1,17 +1,27 @@
+import styles from "./Shop.module.css"
 import image1 from "../../assets/image 8.png"
 import stars from "../../assets/Group 9.png"
 
 
 
-function Product(){
+function Product({product}){
+    const {title,image,category,price,id} = product;
+
+
+   const  handleClick = () => {
+    console.log(product);
+   };
+
     return(
         <div>
-            <div className="hold">
-                        <img src={image1} alt="" className="img"/>
-                        <h4 className="name">CVarrot</h4 >
-                        <p><img src={stars}  alt="" className="stars" /> <span className="review">(3.2% Review)</span></p>
-                        <p><button className="button">Order</button></p>
-                        </div>
+            <div className={styles.hold} onClick={handleClick}>
+                        
+                <img src={image} alt="" className={styles.productImage} />
+                 {/* { <h1 className={styles.title}>{title}</h1> }  */}
+                <p className={styles.price}>{price}</p>
+                 <p><img src={stars}  alt="" className="stars" /> <span className={styles.review}>(3.2% Review)</span></p> 
+                <center><p><button className={styles.btn1}>Order</button></p> </center>
+            </div>
         </div>
     )
 }
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,19 +1,37 @@
-import React,{useState,UseEfect} from "react";
+import React,{useState,UseEfect, useEffect} from "react";
 import Navbar from "../components/common/Navbar";
 import Product from "../components/shop/Product"
+import styles from "../components/shop/Shop.module.css"
 
 function Shop(){
     const [products, setProducts] = useState([]);
+
+    useEffect(function() {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                setProducts(json);
+
+            });
+    }, []);
     return(
-        <div>
-           <div>
-           <Navbar/>
-           </div>
-           <div>
-            <Product/>
-           </div>
-        </div>
-       
-    )
+        <section>
+            <div><Navbar/></div>
+            <div className={styles.container}>
+                { 
+                products.length > 0 ? (
+                    products.map( (product) =>(
+                        <div><Product product={product} /></div>
+                    ))
+                ) : (
+                    <div>Loading.....</div>
+                )
+                 
+                }
+            </div>
+          
+        </section>
+    );
 }
 export default Shop;
